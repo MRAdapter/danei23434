0268aa4fd6ce44cd9c483d800573f23cc2ae2db7
Adapter <2608920335@qq.com>
2015-12-19 16:47:39 +0800

    再次上传

---
+++ 无语/无语/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  无语
+//
+//  Created by apple on 15/12/19.
+//  Copyright © 2015年 Adapter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 无语/无语/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  无语
+//
+//  Created by apple on 15/12/19.
+//  Copyright © 2015年 Adapter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
